7201c30ea0dab5ec00d94b2caf53280d750376dd
VictorSamuel-dev <142364059+VictorSamuel-dev@users.noreply.github.com>
2023-11-05 21:35:03 -0500

    created questionniers and fuctions in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,60 @@ const path = required("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+     type: "input",
+     name: "title",
+     message: "What is the name if your project?",   
+    },
+    {
+     type: "input",
+     name: "description",
+     message: "Could you please describe what your project is?",
+    },
+    {
+     type: "checkbox",
+     name: "license",
+     message: "Please select a icense applicable to this project.",
+     choices: ["MIT", "APACHE2.0", "bOOST1.0", "MPL2.0", "BSD2", "None"],
+    },
+    {
+     type: "input",
+     name: "inquire",
+     message: "List list any project depensencies here",
+    },
+    {
+     type: "input",
+     name: "usage",
+     message: "What language and technologies are used in this project",
+    },
+    {
+     type: "input",
+     name: "creator",
+     message: "What is your GitHub username?",
+    },
+    {
+     type: "input",
+     name: "name",
+     message: "What is your full name?",
+    },
+    {
+     type: "input",
+     name: "email",
+     message: "Whta is a valid email address for you?",
+    },
+    {
+     type: "input",
+     name: "contributors",
+     message: "Please list any contributor. (Use GitHut usernames)",
+     default: "",
+    },
+    {
+     type: "input",
+     name: "test",
+     message: "Provide walkthrough of required test if applicable.",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -16,7 +69,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((response) => {
         console.log("Creating Professional README.md File...");
-        writeToFile("./")
+        writeToFile("./dist/README.md", generateMarkdown({ ...response }));
     });
 }
 
