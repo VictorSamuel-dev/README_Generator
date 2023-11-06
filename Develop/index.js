2f0d0c5140c0a7374c98f9950c1b539063e14497
VictorSamuel-dev <142364059+VictorSamuel-dev@users.noreply.github.com>
2023-11-06 03:27:04 -0500

    commented back in my fas and path in the index.js to debug code

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-// const fs = required("fs");
+const fs = required("fs");
 const inquirer = require("inquirer");
-// const path = required("path");
+const path = required("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
